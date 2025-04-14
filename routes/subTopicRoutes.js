9f00fc9cc1169ffe5d383d56405e990d945c2075
Sara Ashraf <saraa8758@gmail.com>
2025-04-14 17:32:26 +0200

    created Course Module Lesson Topic SubTopic

---
+++ routes/subTopicRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const {
+ 
+  createSubTopic,
+  getSubTopicsByTopic,
+  getSubTopicById,
+  updateSubTopic,
+  deleteSubTopic
+
+} = require("../controllers/subTopicController");
+
+
+router.post("/", createSubTopic);
+router.get("/topic/:topicId", getSubTopicsByTopic);
+router.get("/:id", getSubTopicById);
+router.put("/:id", updateSubTopic);
+router.delete("/:id", deleteSubTopic);
+
+
+module.exports = router;
+
