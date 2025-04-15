94f6179b55e00f6ac2fd80ef2ad9b5b58b33bb8d
Sarah Ashraf <35216590+saraanafady@users.noreply.github.com>
2025-04-15 22:00:12 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const connectDB = require("./db");
-const userRoutes = require("../routes/userRoute");
+const userRoutes = require("./routes/userRoute");
+const courseRoutes = require("./Routes/courseRoutes");
+const moduleRoutes = require("./Routes/moduleRoutes");
+const topicRoutes = require("./Routes/topicRoutes");
+const subTopicRoutes = require("./Routes/subTopicRoutes");
+const lessonRoutes = require("./Routes/lessonRoutes");
+const userRoutes = require("./Routes/userRoute");
 const app = express();
 const cors = require("cors");
 
@@ -17,6 +23,12 @@ app.use(express.json());
 
 connectDB().then(() => {
   app.use("/api/users", userRoutes);
+  app.use("/api/courses", courseRoutes);
+  app.use("/api/modules", moduleRoutes);
+  app.use("/api/topics", topicRoutes);
+  app.use("/api/subtopics", subTopicRoutes);
+  app.use("/api/lessons", lessonRoutes);
+
 
   app.use((err, res) => {
     console.error(err.stack);
