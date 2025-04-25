c5be707b4ae4003aa26a87996523045a4abdfe25
Sarah Ashraf <35216590+saraanafady@users.noreply.github.com>
2025-04-25 20:55:20 +0300

    fix:vercal

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// api/index.js
+const serverless = require("serverless-http");
+const app = require("./server");
+
+module.exports = serverless(app);
--- api/server.js
+++ api/server.js
@@ -62,3 +62,5 @@ connectDB().then(() => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 });
+
+module.exports = app;
